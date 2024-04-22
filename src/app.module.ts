9b12bc1449ffab65cbef0de339f7b17fac8bdcc9
Gaël Mayer <46091558+Gmayer111@users.noreply.github.com>
2024-04-22 16:59:08 +0200

    chore: add new release conf (#1)
    * chore: add new release conf
    
    * chore: add PR template
    
    * chore: add DB and .env config
    
    * chore: update types/express

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: 'gael',
+      password: 'secret',
+      database: 'passionbd-db',
+      models: [],
+    }),
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
