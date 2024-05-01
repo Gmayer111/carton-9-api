19c8df31368b4c5b64d872e5b59d46376279e75f
Gmayer111 <gaelmayer@yahoo.fr>
2024-05-01 21:40:21 +0200

    feat: create dto

--- src/comics/comics.module.ts
+++ src/comics/comics.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ComicsService } from './comics.service';
 import { ComicsController } from './comics.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Comic } from './models/comic.models';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Comic])],
   controllers: [ComicsController],
   providers: [ComicsService],
 })
--- src/publishers/publishers.module.ts
+++ src/publishers/publishers.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PublishersService } from './publishers.service';
 import { PublishersController } from './publishers.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Publisher } from './models/publisher.models';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Publisher])],
   controllers: [PublishersController],
   providers: [PublishersService],
 })
