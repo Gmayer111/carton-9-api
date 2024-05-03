68cd40a990b33a3d8e4ff4ce6b9d1fec8e9c2899
Gmayer111 <gaelmayer@yahoo.fr>
2024-05-03 19:14:12 +0200

    fix: remevo unused role ressource

--- src/roles/roles.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { Role } from './models/role.model';
-
-@Module({
-  imports: [SequelizeModule.forFeature([Role])],
-})
-export class RoleModule {}
