899db28a867152c68732d64170a2b1a83bdbcc08
Gmayer111 <gaelmayer@yahoo.fr>
2024-05-06 16:07:19 +0200

    fix: add models to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,13 @@ import { CategoriesModule } from './categories/categories.module';
 import { AuthorsModule } from './authors/authors.module';
 import { PublishersModule } from './publishers/publishers.module';
 import { CollectionsModule } from './collections/collections.module';
+import { Category } from './categories/models/category.models';
+import { Comic } from './comics/models/comic.models';
+import { Collection } from './collections/models/collection.models';
+import { Author } from './authors/models/author.models';
+import { ComicAuthor } from './comics-authors/models/comic-author.model';
+import { Publisher } from './publishers/models/publisher.models';
+import { ComicCategory } from './comics-categories/models/comic-category.models';
 
 @Module({
   imports: [
@@ -23,7 +30,16 @@ import { CollectionsModule } from './collections/collections.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      models: [User],
+      models: [
+        User,
+        Comic,
+        Collection,
+        Author,
+        Category,
+        Publisher,
+        ComicAuthor,
+        ComicCategory,
+      ],
     }),
     UserModule,
     ComicsModule,
