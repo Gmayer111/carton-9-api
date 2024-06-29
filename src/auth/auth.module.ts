78e2845b39eb6ef7e3ba167fa3b8ed0eeaf43659
Gaël Mayer <46091558+Gmayer111@users.noreply.github.com>
2024-06-29 22:13:43 +0200

    feat: create auth tests (#13)
    * fix: remove unused imports
    
    * feat: add auth controller & service tests

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
-import { PassportModule } from '@nestjs/passport';
 
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constant';
 import { AuthController } from './auth.controller';
-import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [
     UserModule,
-    PassportModule,
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
