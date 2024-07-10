be25f6d1dc533cb717b4dbda2390b08ed9fa18e3
Gmayer111 <gaelmayer@yahoo.fr>
2024-07-10 11:02:33 +0200

    feat: create controller / service & tests

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -10,24 +10,31 @@ import {
 import { AuthorsService } from './authors.service';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { RoleEnum } from 'src/authorization/enums/role.enum';
+import { Roles } from 'src/authorization/roles.decorator';
 
 @ApiTags('Authors')
+@ApiBearerAuth()
+@Roles(RoleEnum.admin)
 @Controller('authors')
 export class AuthorsController {
   constructor(private readonly authorsService: AuthorsService) {}
 
   @Post()
+  @Roles(RoleEnum.admin, RoleEnum.user)
   create(@Body() createAuthorDto: CreateAuthorDto) {
     return this.authorsService.create(createAuthorDto);
   }
 
   @Get()
+  @Roles(RoleEnum.admin, RoleEnum.user)
   findAll() {
     return this.authorsService.findAll();
   }
 
   @Get(':id')
+  @Roles(RoleEnum.admin, RoleEnum.user)
   findOne(@Param('id') id: string) {
     return this.authorsService.findOne(+id);
   }
@@ -38,6 +45,7 @@ export class AuthorsController {
   }
 
   @Delete(':id')
+  @Roles(RoleEnum.admin, RoleEnum.user)
   remove(@Param('id') id: string) {
     return this.authorsService.remove(+id);
   }
--- src/authors/authors.service.spec.ts
+++ src/authors/authors.service.spec.ts
@@ -1,18 +1,79 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/sequelize';
 import { AuthorsService } from './authors.service';
+import { Author } from './models/author.models';
 
 describe('AuthorsService', () => {
   let service: AuthorsService;
+  let model: Author;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthorsService],
+      providers: [
+        AuthorsService,
+        {
+          provide: getModelToken(Author),
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            findByPk: jest.fn(),
+            update: jest.fn(),
+            destroy: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthorsService>(AuthorsService);
+    model = module.get(getModelToken(Author));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a author', async () => {
+      const data = { userName: 'Hergé' };
+      const modelSpy = jest.spyOn(model, 'create' as any);
+      await service.create(data as any);
+      expect(modelSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should find all authors', async () => {
+      const modelSpy = jest.spyOn(model, 'findAll' as any);
+      await service.findAll();
+      expect(modelSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should find one author', async () => {
+      const modelSpy = jest.spyOn(model, 'findByPk' as any);
+      await service.findOne(1);
+      expect(modelSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('update', () => {
+    it('should update one author', async () => {
+      const modelSpy = jest.spyOn(model, 'findByPk' as any).mockResolvedValue({
+        id: 1,
+        update: jest.fn(),
+      });
+      await service.update(1, { userName: 'Hergé' });
+      expect(modelSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('destroy', () => {
+    it('should destroy one author', async () => {
+      const modelSpy = jest.spyOn(model, 'destroy' as any);
+      await service.remove(1);
+      expect(modelSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
