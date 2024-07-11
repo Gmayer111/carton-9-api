c297c9e3f718e0660c6fb1504824925ff65b942b
Gmayer111 <gaelmayer@yahoo.fr>
2024-07-11 09:42:24 +0200

    feat: add export COmicsAuthorsService

--- src/comics-authors/comics-authors.module.ts
+++ src/comics-authors/comics-authors.module.ts
@@ -8,5 +8,6 @@ import { ComicAuthor } from './models/comic-author.model';
   imports: [SequelizeModule.forFeature([ComicAuthor])],
   controllers: [ComicsAuthorsController],
   providers: [ComicsAuthorsService],
+  exports: [ComicsAuthorsService],
 })
 export class ComicsAuthorsModule {}
