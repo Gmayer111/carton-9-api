2b5067ed5817130cdb585f6807770f2fa77de8ed
Gmayer111 <gaelmayer@yahoo.fr>
2024-08-22 16:20:31 +0200

    feat: add cors to main

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
     .setTitle('Passions BD')
