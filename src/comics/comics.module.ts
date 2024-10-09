d307e9cd7ffa319c2ba89e7232a8ef5a11db9cfc
Gmayer111 <gaelmayer@yahoo.fr>
2024-10-09 11:11:14 +0200

    feat: add comicAuthors model to app & comics module

--- src/comics/comics.module.ts
+++ src/comics/comics.module.ts
@@ -4,9 +4,16 @@ import { ComicsController } from './comics.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Comic } from './models/comic.models';
 import { CollectionsModule } from 'src/collections/collections.module';
+import { Collection } from 'src/collections/models/collection.models';
+import { Publisher } from 'src/publishers/models/publisher.models';
+import { ComicsAuthorsModule } from 'src/comics/comics-authors/comics-authors.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Comic]), CollectionsModule],
+  imports: [
+    SequelizeModule.forFeature([Comic, Collection, Publisher]),
+    CollectionsModule,
+    ComicsAuthorsModule,
+  ],
   controllers: [ComicsController],
   providers: [ComicsService],
   exports: [ComicsService],
